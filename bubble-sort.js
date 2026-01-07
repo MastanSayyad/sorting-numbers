e2bd3cbfd739817287634ffd48fc61576229cbb7
Mastan Sayyad <sayyadmastan78@gmail.com>
2026-01-08 02:28:20 +0530

    create: sorting algos

--- bubble-sort.js
+++ bubble-sort.js
@@ -4,7 +4,9 @@ function bubbleSort(arr) {
 
     for (let i = 0; i < n - 1; i++) {
         for (let j = 0; j < n - i - 1; j++) {
-            
+            // 5 2 8 1 9 3
+            // 2 5 8 1 9 3
+            // 1 2 3 5 8 9
             if (arr[j] > arr[j + 1]) { // If current number is greater than next, swap them
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
